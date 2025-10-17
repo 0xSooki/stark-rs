e7a7737c5008e3270c53f9882bc57f1c6d58b617
0xSooki <0xsooki@gmail.com>
2025-10-17 19:42:07 +0200

    add Lagrange interpolation for Polynomail

--- src/poly.rs
+++ src/poly.rs
@@ -209,14 +209,51 @@ impl Polynomial {
         result
     }
 
-    pub fn eval(poly: &Self, x: &FieldElement) -> FieldElement {
+    pub fn eval(&self, x: &FieldElement) -> FieldElement {
         let mut xi = x.field.one();
         let mut val = x.field.zero();
-        for c in &poly.coeffs {
+        for c in &self.coeffs {
             val = val + (*c) * xi;
             xi = xi * (*x);
         }
         val
+    }
+
+    pub fn eval_domain(&self, domain: &Vec<FieldElement>) -> Vec<FieldElement> {
+        domain
+            .iter()
+            .map(|x| Self::eval(self, x))
+            .collect::<Vec<FieldElement>>()
+    }
+
+    pub fn interpolate_domain(
+        domain: &Vec<FieldElement>,
+        values: &Vec<FieldElement>,
+    ) -> Polynomial {
+        assert!(domain.len() == values.len());
+        assert!(domain.len() > 0);
+        let field = domain[0].field;
+        let mut x = Polynomial {
+            coeffs: vec![field.zero(), field.one()],
+            field: field,
+        };
+        let mut acc = Polynomial {
+            coeffs: vec![],
+            field: field,
+        };
+        for i in 0..domain.len() {
+            let mut prod = Polynomial {
+                coeffs: vec![values[i]],
+                field: field,
+            };
+            for j in 0..domain.len() {
+                if j == i {
+                    continue;
+                }
+                prod = prod
+            }
+        }
+        acc
     }
 
     fn assert_same_field(p1: &Polynomial, p2: &Polynomial) {
