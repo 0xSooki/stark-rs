859208cb972535b62f1cf0c19d1b1250fd5e1b95
0xSooki <0xsooki@gmail.com>
2025-10-18 16:34:33 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use crate::ff::FiniteField;
 mod ff;
-pub mod poly;
 mod utils;
-use crate::poly::Polynomial;
+pub mod univariate;
+use crate::univariate::Polynomial;
 const P: u64 = 998244353; // 2^31 - 1 (Mersenne prime)
 
 fn main() {
     let f = FiniteField::new(P);
     let root = f.prim_nth_root(8);
-    let pp = poly::Polynomial::new(vec![], f);
+    let pp = Polynomial::new(vec![], f);
 
     println!("{:?}", pp);
 }
