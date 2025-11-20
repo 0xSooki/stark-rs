dbe9fb3f5d67948c73352946a129ccaeaab8df02
0xSooki <0xsooki@gmail.com>
2025-11-20 15:22:07 +0100

    add computation trace

---
+++ src/trace.rs
@@ -0,0 +1,35 @@
+use crate::ff::{FieldElement, FiniteField};
+
+#[derive(Clone)]
+pub struct Trace {
+    pub trace: Vec<Vec<i128>>,
+    pub num_columns: usize,
+}
+
+impl Trace {
+    pub fn new(trace: &Vec<Vec<i128>>) -> Trace {
+        Trace {
+            trace: trace.to_vec(),
+            num_columns: trace[0].len(),
+        }
+    }
+
+    pub fn get_row(&self, i: usize) -> Option<&Vec<i128>> {
+        self.trace.get(i)
+    }
+
+    pub fn get_col(&self, j: usize) -> Vec<i128> {
+        self.trace.iter().map(|r| r[j]).collect()
+    }
+
+    pub fn get(&self, i: usize, j: usize) -> Option<i128> {
+        self.trace.get(i)?.get(j).copied()
+    }
+
+    pub fn to_field_elements(&self, field: FiniteField) -> Vec<Vec<FieldElement>> {
+        self.trace
+            .iter()
+            .map(|r| r.iter().map(|&e| field.new_element(e as u64)).collect())
+            .collect()
+    }
+}
